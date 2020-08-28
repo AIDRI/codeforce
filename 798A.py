c3d26a757e2d820b3d339d135c71818fd6dfa667
AIDRI <60047183+AIDRI@users.noreply.github.com>
2020-08-28 23:29:03 +0200

    Create 798A.py

---
+++ 798A.py
@@ -0,0 +1,17 @@
+s = input()
+count = 0
+possible = True
+for i in range(int(len(s) / 2)):
+    if s[i] != s[len(s) - i - 1]:
+        count += 1
+        if count > 1:
+            possible = False
+            break
+ 
+if len(s) % 2 == 1 and count == 0:
+    count += 1
+ 
+if possible and count == 1:
+    print('YES')
+else:
+    print('NO')
